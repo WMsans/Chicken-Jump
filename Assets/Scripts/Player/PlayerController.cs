152bfaf86401c9b697429328d50a742ed1f3c570
WMsans <ggajy123@gmail.com>
2024-08-03 15:06:34 -0700

    Rotations and falling

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,14 +7,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public static PlayerController Instance { get; private set; }
-    public Enums.PlayerState currentState { get; set; }
-    Dictionary<Enums.PlayerState, PlayerBaseState> states = new Dictionary<Enums.PlayerState, PlayerBaseState>
+    public Enums.PlayerState currentState { get; private set; }
+    Dictionary<Enums.PlayerState, PlayerBaseState> states = new()
     {
         {Enums.PlayerState.Normal, new PlayerNormalState() },
         {Enums.PlayerState.Stiff, new PlayerStiffState() }
     };
     [Header("Player Movement")]
-    public Transform forcePoint;
     public float m_JumpForce = 400f;
     [Range(0, 1f)]public float m_MovementSmoothing = .05f;    // How much to smooth out the movement
     public float maxHorizontalSpeed;                          // The walk speed of the player
@@ -23,15 +22,28 @@ public class PlayerController : MonoBehaviour
     public bool m_AirControl = false;                         // Whether or not a player can steer while jumping;
     public LayerMask m_WhatIsGround;                          // A mask determining what is ground to the character
     public Transform m_GroundCheck;                           // A position marking where to check if the player is grounded.
-    public float m_GroundBuff;                                // Distance above ground is alloed to jump
+    public float m_GroundedRadius;                            // The distance of to check the ground
+    public float m_GroundBuff;                                // The distance to jump before touching the ground
     public int m_CoyoteTime;                                  // Coyote time 
     [Range(0, 1f)] public float lowJumpMultiplier = 2.0f;
     public float fallMultiplier = 2.5f;
     public float maxFallSpeed;
-    public bool Grounded { get; set; } = false;
+    public bool ReadyToJump { get; set; } = false;
     public bool keyJump { get; private set; }
     public bool keyJumpDown { get; private set; }
     public float keyHor { get; private set; }
+
+    [Header("Player Rotation")]
+    public float maxRotateSpeed;
+    public float maxReturnSpeed;
+    [Range(0, 1f)]public float returnSpeedSmoothing;
+    public float rotateAccel;
+    public float rotateDecel;
+    public float fallingAccel;
+
+    [Header("Arms and Legs")]
+    public Collider2D arms;
+    public bool ArmTouchingGround { get { return arms.IsTouchingLayers(m_WhatIsGround); } }
     void Awake()
     {
         if (Instance != null)
@@ -84,5 +96,12 @@ public class PlayerController : MonoBehaviour
     public PlayerBaseState GetStateInstance(Enums.PlayerState state)
     {
         return states[state];
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(m_GroundCheck.position, m_GroundedRadius);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(m_GroundCheck.position, m_GroundedRadius + m_GroundBuff);
     }
 }
--- Assets/Scripts/Player/PlayerStates.cs
+++ Assets/Scripts/Player/PlayerStates.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,12 +11,14 @@ public abstract class PlayerBaseState
 }
 public class PlayerNormalState : PlayerBaseState
 {
-    bool m_Grounded;
+    bool readyToJump;
+    bool Grounded;
+    bool fallingToGround;
     bool m_FacingRight = true;
     bool m_AirControl;
     Transform m_GroundCheck;
+    float m_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
     float m_GroundBuff;
-    const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
     LayerMask m_WhatIsGround;
     float maxHorizontalSpeed;
     float accelerationSpeed;
@@ -25,60 +26,89 @@ public class PlayerNormalState : PlayerBaseState
     float m_JumpForce;
     float fallMultiplier;
     float lowJumpMultiplier;
+    float maxFallSpeed;
+    float rotateAccelerationSpeed;
+    float rotateDecelerationSpeed;
+    float fallingAccelerationSpeed;
+    float maxRotateSpeed;
+    float maxReturnSpeed;
+    float returnSpeedSmoothing;
+    float rotateSpeed;
 
     Rigidbody2D m_Rigidbody2D;
     int _coyoteTimer = 0;
+    int _jumpingTimer = 0;
     public override void EnterState(PlayerController player)
     {
         m_Rigidbody2D = player.GetComponent<Rigidbody2D>();
         m_GroundCheck = player.m_GroundCheck;
         m_WhatIsGround = player.m_WhatIsGround;
+        m_GroundedRadius = player.m_GroundedRadius;
         m_GroundBuff = player.m_GroundBuff;
-        m_AirControl=  player.m_AirControl;
+        m_AirControl =  player.m_AirControl;
         maxHorizontalSpeed = player.maxHorizontalSpeed;
         accelerationSpeed = player.accel;
         decelerationSpeed = player.decel;
         m_JumpForce = player.m_JumpForce;
         fallMultiplier = player.fallMultiplier;
         lowJumpMultiplier = player.lowJumpMultiplier;
+        maxFallSpeed = player.maxFallSpeed;
+        rotateAccelerationSpeed = player.rotateAccel;
+        rotateDecelerationSpeed = player.rotateDecel;
+        fallingAccelerationSpeed = player.fallingAccel;
+        maxRotateSpeed = player.maxRotateSpeed;
+        maxReturnSpeed = player.maxReturnSpeed;
+        returnSpeedSmoothing = player.returnSpeedSmoothing;
 
-        m_Grounded = player.Grounded;
+        readyToJump = player.ReadyToJump;
     }
     public override void UpdateState(PlayerController player)
     {
-        
-
+        if (player.keyJumpDown)
+        {
+            _jumpingTimer = player.m_CoyoteTime;
+        }
     }
     public override void FixedUpdateState(PlayerController player)
     {
-        bool wasGrounded = m_Grounded;
-        m_Grounded = false;
-        player.Grounded = m_Grounded;
+        bool wasGrounded = readyToJump;
+        readyToJump = false;
+        fallingToGround = false;
+        player.ReadyToJump = readyToJump;
 
-        // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
-        // This can be done using layers instead but Sample Assets will not overwrite your project settings.
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(new Vector2(m_GroundCheck.position.x, m_GroundCheck.position.y - m_GroundBuff), k_GroundedRadius, m_WhatIsGround);
+        // The player is ready to jump if a circlecast to the groundcheck position hits anything designated as ground
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(new Vector2(m_GroundCheck.position.x, m_GroundCheck.position.y), m_GroundedRadius + m_GroundBuff, m_WhatIsGround);
         for (int i = 0; i < colliders.Length; i++)
         {
             if (colliders[i].isActiveAndEnabled && colliders[i].gameObject != player.gameObject)
             {
-                m_Grounded = true;
-                player.Grounded = m_Grounded;
+                readyToJump = true;
+                player.ReadyToJump = readyToJump;
                 _coyoteTimer = player.m_CoyoteTime;
             }
         }
 
-        _coyoteTimer = Mathf.Max(_coyoteTimer - 1, 0);
+        // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
+        colliders = Physics2D.OverlapCircleAll(new Vector2(m_GroundCheck.position.x, m_GroundCheck.position.y), m_GroundedRadius, m_WhatIsGround);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].isActiveAndEnabled && colliders[i].gameObject != player.gameObject)
+            {
+                fallingToGround = true;
+            }
+        }
 
-        Move(player.keyHor * Time.fixedDeltaTime, player.keyJumpDown, player.keyJump, player);
+        _coyoteTimer = Mathf.Max(_coyoteTimer - 1, 0);
+        _jumpingTimer = Mathf.Max(_jumpingTimer - 1, 0);
+
+        Move(player.keyHor * Time.fixedDeltaTime, _jumpingTimer > 0, player.keyJump, player);
     }
     void Move(float move, bool jumpDown, bool jump, PlayerController player)
     {
-        //only control the player if grounded or airControl is turned on
-        if (m_Grounded || m_AirControl)
+        // only control the player if grounded or airControl is turned on
+        if (readyToJump || m_AirControl)
         {
-
-            // Reduce the speed by the crouchSpeed multiplier
+            #region Player Horizontal Movement
             if ((Mathf.Abs(m_Rigidbody2D.velocity.x + _sign(move) * accelerationSpeed) <= maxHorizontalSpeed || (_sign(move) != _sign(m_Rigidbody2D.velocity.x) && Mathf.Abs(move) > 0.001f)))
             {
                 m_Rigidbody2D.velocity += _sign(move) * accelerationSpeed * Vector2.right;
@@ -100,8 +130,55 @@ public class PlayerNormalState : PlayerBaseState
                     else m_Rigidbody2D.velocity -= decelerationSpeed * Vector2.right;
                 }
             }
-            if (player.maxFallSpeed < 0)
-                m_Rigidbody2D.velocity = new(m_Rigidbody2D.velocity.x, Mathf.Max(m_Rigidbody2D.velocity.y, player.maxFallSpeed));
+            #endregion
+
+            #region Rotation
+            if ((Mathf.Abs(rotateSpeed + _sign(move) * rotateAccelerationSpeed) <= maxRotateSpeed || (_sign(move) != _sign(rotateSpeed) && Mathf.Abs(move) > 0.001f)))
+            {
+                rotateSpeed += _sign(move) * rotateAccelerationSpeed;
+            }
+            else if ((Mathf.Abs(rotateSpeed + _sign(move) * rotateAccelerationSpeed) > maxRotateSpeed && Mathf.Abs(rotateSpeed) < maxRotateSpeed))
+            {
+                rotateSpeed = _sign(move) * maxRotateSpeed;
+            }
+            rotateSpeed = Mathf.Clamp(rotateSpeed, -maxRotateSpeed, maxRotateSpeed);
+            if(Mathf.Abs(move) <= 0.001f && m_Rigidbody2D.velocity.y > 0 && jump) // Player not moving, Holding space and rising
+            {
+                // Start Turn Back
+                var _playerAngleDiff = Mathf.DeltaAngle(player.transform.rotation.z * Mathf.Rad2Deg, 0f);
+                rotateSpeed = _playerAngleDiff * (1f - returnSpeedSmoothing);
+
+                /*if (rotateSpeed + rotateAccelerationSpeed > Mathf.Abs(_playerAngleDiff * (1f - returnSpeedSmoothing)))
+                {
+                    rotateSpeed = _playerAngleDiff * (1f - returnSpeedSmoothing);
+                }
+                else
+                {
+                    rotateSpeed += rotateAccelerationSpeed * _sign(_playerAngleDiff);
+                }*/
+                rotateSpeed = Mathf.Clamp(rotateSpeed, -maxReturnSpeed, maxReturnSpeed);
+            }
+            else if(Mathf.Abs(move) <= 0.001f && fallingToGround && !player.ArmTouchingGround)
+            {
+                var _playerAngleDiff = Mathf.DeltaAngle(player.transform.rotation.z * Mathf.Rad2Deg, 0f);
+                rotateSpeed += _reverseSign(_playerAngleDiff) * fallingAccelerationSpeed;
+            }
+            else if ((Mathf.Abs(move) <= 0.001f && m_Rigidbody2D.velocity.y > 0 && !fallingToGround) || Mathf.Abs(rotateSpeed) > maxRotateSpeed || player.ArmTouchingGround) // Stop Rotation
+            {
+                if (Mathf.Abs(rotateSpeed) <= rotateDecelerationSpeed)
+                {
+                    rotateSpeed = 0f;
+                }
+                else
+                {
+                    if (rotateSpeed < 0) rotateSpeed += rotateDecelerationSpeed;
+                    else rotateSpeed -= rotateDecelerationSpeed;
+                }
+            }
+            player.transform.Rotate(0f, 0f, rotateSpeed);
+            #endregion
+            if (maxFallSpeed < 0)
+                m_Rigidbody2D.velocity = new(m_Rigidbody2D.velocity.x, Mathf.Max(m_Rigidbody2D.velocity.y, maxFallSpeed));
             // If the input is moving the player right and the player is facing left...
             if (move > 0 && !m_FacingRight)
             {
@@ -114,28 +191,32 @@ public class PlayerNormalState : PlayerBaseState
                 // ... flip the player.
                 Flip(player);
             }
-            // Fall faster when falling
-            if (m_Rigidbody2D.velocity.y < 0)
-            {
-                m_Rigidbody2D.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
-            }
-            // Fall faster after releasing jump key
-            else if (!jump)
-            {
-                m_Rigidbody2D.velocity *= new Vector2(1f, lowJumpMultiplier);
-                //m_Rigidbody2D.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
-            }
-            // If the player should jump
-            if ((m_Grounded || _coyoteTimer > 0) && jumpDown)
-            {
-                
-                // Add a vertical force to the player.
-                m_Grounded = false;
-                m_Rigidbody2D.velocity *= Vector2.right;
-                m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
-            }
+        }
+        // Fall faster when falling
+        if (m_Rigidbody2D.velocity.y < 0)
+        {
+            m_Rigidbody2D.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        // Fall faster after releasing jump key
+        else if (!jump)
+        {
+            m_Rigidbody2D.velocity *= new Vector2(1f, lowJumpMultiplier);
+        }
+        // If the player should jump
+        if ((readyToJump || _coyoteTimer > 0) && jumpDown)
+        {
+            // Reset the jumping timer
+            _jumpingTimer = 0;
+            // Add a vertical force to the player.
+            readyToJump = false;
+            m_Rigidbody2D.velocity *= Vector2.right;
+            m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
         }
     }
+    /// <summary>
+    /// Flips the player horizontally
+    /// </summary>
+    /// <param name="player"></param>
     void Flip(PlayerController player)
     {
         // Switch the way the player is labelled as facing.
@@ -146,10 +227,24 @@ public class PlayerNormalState : PlayerBaseState
         theScale.x *= -1;
         player.transform.localScale = theScale;
     }
+    /// <summary>
+    /// Returns the sign of the number including zero
+    /// </summary>
+    /// <param name="x"></param>
+    /// <returns>The sign of the number</returns>
     float _sign(float x)
     {
-        if (Mathf.Abs(x) < 0.0001) return 0;
+        if (Mathf.Abs(x) < 0.001) return 0;
         return Mathf.Sign(x);
+    }
+    /// <summary>
+    /// Get the sign of the number and reverse it, including zero
+    /// </summary>
+    /// <param name="x"></param>
+    /// <returns>The number's reversed sign</returns>
+    float _reverseSign(float x)
+    {
+        return _sign(x) * -1;
     }
     public override void ExitState(PlayerController player)
     {
